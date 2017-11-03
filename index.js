99552b4f908294ac1a217d224e6f4fc5b6aed433
TheMaster99 <las6731@rit.edu>
2017-11-02 21:09:25 -0400

    Begin customizing pages, and create all main pages

--- index.js
+++ index.js
@@ -1,14 +1,65 @@
 var express = require('express')
   , logger = require('morgan')
   , app = express()
-  , template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
+  , jade = require('jade')
+  , homePage = jade.compileFile(__dirname + '/source/templates/home.jade')
+  , aboutPage = jade.compileFile(__dirname + '/source/templates/about.jade')
+  , standingsPage = jade.compileFile(__dirname + '/source/templates/standings.jade')
+  , cupPage = jade.compileFile(__dirname + '/source/templates/cup.jade')
+  , teamsPage = jade.compileFile(__dirname + '/source/templates/teams.jade')
+  , playersPage = jade.compileFile(__dirname + '/source/templates/players.jade')
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
 
 app.get('/', function (req, res, next) {
   try {
-    var html = template({ title: 'Home' })
+    var html = homePage({ title: 'Home' })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.get('/about', function (req, res, next) {
+  try {
+    var html = aboutPage({ title: 'About' })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.get('/standings', function (req, res, next) {
+  try {
+    var html = standingsPage({ title: 'League Standings' })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.get('/cup', function (req, res, next) {
+  try {
+    var html = cupPage({ title: 'Open Cup' })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.get('/teams', function (req, res, next) {
+  try {
+    var html = teamsPage({ title: 'Teams' })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.get('/players', function (req, res, next) {
+  try {
+    var html = playersPage({ title: 'Players' })
     res.send(html)
   } catch (e) {
     next(e)
